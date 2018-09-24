82451a37a8c39c4d9a349a4a053ef6194f22c038
AB <annabelle.macfarlane@gmail.com>
2018-09-24 13:21:47 +1000

    created Gitingore and brought in prefab objects

---
+++ Assets/Scripts/ClickToAnim.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickToAnim : MonoBehaviour
+{
+
+    public Animator animator;
+    public string MouseDown = "MouseDown";
+    private Camera camera;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        RaycastHit hitInfo;
+        Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+
+        if (Physics.Raycast(ray, out hitInfo) && hitInfo.collider.gameObject == gameObject)
+        {
+            if(Input.GetMouseButtonDown(0))
+            {
+                animator.SetTrigger(MouseDown);
+            }
+
+        }
+
+    }
+}
