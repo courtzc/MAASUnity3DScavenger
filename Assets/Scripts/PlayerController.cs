f43b0fa4ad6bd84307a2ac4c7b64559e9506241d
courtzc <32031285+courtzc@users.noreply.github.com>
2019-04-10 08:53:47 +1000

    changing testscene and adding player controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Vector3 pos;
+    public float speed = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pos = transform.position;
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            pos.z += speed * Time.deltaTime;
+        }
+
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            pos.z -= speed * Time.deltaTime;
+        }
+
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            pos.x -= speed * Time.deltaTime;
+        }
+
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            pos.x += speed * Time.deltaTime;
+        }
+
+        transform.position = pos;
+    }
+}
